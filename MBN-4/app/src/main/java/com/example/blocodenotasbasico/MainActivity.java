c399a79458a587b9037b011d3167bdf8971eca30
Gabriel Nogueira Lages <78828285+Gabriel-NL@users.noreply.github.com>
2021-09-11 16:31:06 -0500

    MBN-4
    Consegui criar um metodo para gerar os botoes e adicionar eventos para cada um
    Mas, eles so aparecem quando o app se inicia, não quando transito entre fragmentos. Mais conhecimento requerido

---
+++ MBN-4/app/src/main/java/com/example/blocodenotasbasico/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.blocodenotasbasico;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.blocodenotasbasico.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    private List<Button> caixaDeBotoes = new ArrayList<Button>();
+    private SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        /*
+        sem isto, o fragment first não funciona
+        *
+        *
+        * */
+
+        //aqui embaixo, as notas são armazenadas
+        conteudo();
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+
+
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public void redefinirNome(View view){
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
+    public void conteudo(){
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layoutFragment);
+        for (int i=0;i< 2;i++ ){
+            String nomebotao= "botao "+i;
+            Button btn = new Button(this);
+            btn.setText("batata assada");
+            btn.setTextColor(Color.BLACK);
+            //btn.setId(Integer.parseInt(nomebotao));
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    btn.setText("batata queimada");
+                }
+            });
+            caixaDeBotoes.add(btn);
+            layout.addView(btn);
+
+        }
+    }
+}
